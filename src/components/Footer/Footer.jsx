8b5fc652707cc3b34bd416211d414639fbd265a6
keerthiux <vksaikeerthi@gmail.com>
2026-06-28 10:05:22 +0530

    fixing year issue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <a href="mailto:vksaikeerthi@gmail.com">vksaikeerthi@gmail.com</a>
-      <p>© 2025 Sai Keerthi. All rights reserved. </p>
+      <p>© {new Date().getFullYear()} Sai Keerthi. All rights reserved. </p>
     </footer>
   );
 };
